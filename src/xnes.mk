78126a840f75be686cb996e5a2d3402f0bbd7e93
JianjunJiang <8192542@qq.com>
2024-10-10 12:38:35 +0800

    [mapper]move mapper to a directory

--- src/xnes.mk
+++ src/xnes.mk
@@ -6,5 +6,7 @@ XNES_DIR = $(realpath $(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
 #
 # xnes
 #
-INCDIRS		+=	$(XNES_DIR)
-SRCDIRS		+=	$(XNES_DIR)
+INCDIRS		+=	$(XNES_DIR) \
+				$(XNES_DIR)/mapper
+SRCDIRS		+=	$(XNES_DIR) \
+				$(XNES_DIR)/mapper
